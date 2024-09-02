f94f4ee8a85d9a69a7f110e49b9f9a9e0101da0e
Aleksander <mr.alexandrlebedev@mail.ru>
2024-09-02 13:21:25 +0300

    Добавил гигачат; перенос постгреса на старую либу, чтобы избежать UUID, рефактор create_chain для большей гибкости

--- libs/llm_chat.py
+++ libs/llm_chat.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 import psycopg
+from langchain_community.chat_models import GigaChat
 from openai import RateLimitError
 
 from langchain_core.runnables import RunnableBinding
@@ -12,7 +13,8 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import create_retrieval_chain, create_history_aware_retriever
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_postgres import PostgresChatMessageHistory
+from langchain_community.chat_message_histories import PostgresChatMessageHistory
+from langchain_postgres import PostgresChatMessageHistory as Psql
 
 from vectorstore import load_vectorstore
 
@@ -22,6 +24,7 @@ vec_store_save_path = "FAISS_store_08_24.db"
 # Определение корневого каталога проекта
 PROJECT_ROOT = Path(__file__).parent.parent.absolute()
 VEC_STORE_LOAD_PATH = Path.joinpath(PROJECT_ROOT, vec_store_save_path)
+GIGACHAT_KEY = os.environ.get('GIGACHAT_KEY')
 API_KEY = os.environ.get("OPEN_ROUTER_KEY")
 API_KEY2 = os.environ.get("OPEN_ROUTER_KEY_2")
 API_BASE = "https://openrouter.ai/api/v1"
@@ -34,28 +37,46 @@ PSQL_PASSWORD = os.environ.get("PSQL_PASSWORD")
 PGSQL_HOST = os.environ.get("PGSQL_HOST")
 PGSQL_DATABASE = os.environ.get("PGSQL_DATABASE")
 CONN_STR2BD = f"dbname={PGSQL_DATABASE} user={PSQL_USERNAME} password={PSQL_PASSWORD} host={PGSQL_HOST}"
-TABLE='chat_history'
+TABLE = 'chat_history'
 sync_connection = psycopg.connect(CONN_STR2BD)
 
 # названия моделей
 llama_3_1_8b = "meta-llama/llama-3.1-8b-instruct:free"
-hermes       = "nousresearch/hermes-3-llama-3.1-405b"
-zephyr       = "huggingfaceh4/zephyr-7b-beta:free"
-openchat     = "openchat/openchat-7b:free"
-phi3         = "microsoft/phi-3-medium-128k-instruct:free"
-gemma2       = "google/gemma-2-9b-it:free"
-qwen2        = "qwen/qwen-2-7b-instruct:free"
-capybara     = "nousresearch/nous-capybara-7b:free"
-mythomist    = "gryphe/mythomist-7b:free"
+hermes = "nousresearch/hermes-3-llama-3.1-405b"
+zephyr = "huggingfaceh4/zephyr-7b-beta:free"
+openchat = "openchat/openchat-7b:free"
+phi3 = "microsoft/phi-3-medium-128k-instruct:free"
+gemma2 = "google/gemma-2-9b-it:free"
+qwen2 = "qwen/qwen-2-7b-instruct:free"
+capybara = "nousresearch/nous-capybara-7b:free"
+mythomist = "gryphe/mythomist-7b:free"
+
+SYSTEM_PROMT = """Ты - чат-бот Енот, и работаешь в чате сети магазинов хороших продуктов "Жизньмарт",
+    твоя функция - стараться ответить на любой вопрос клиента про работу магазинов "Жизьмарт".
+    Используй в ответах только русский язык! Не отвечай на английском! 
+    Если вопрос не касается контекста, то вежливо и дружелюбно переведи тему и расскажи про Живчики Жизьмарта.
+
+    {context}
+
+    Используй только этот контекст, чтобы ответить на последний вопрос.
+    Если ответа нет в контексте, просто позитивно поддержи диалог на тему Жизньмарта!
+    Если клиент поздоровался с тобой, но НЕ ЗАДАЛ вопрос, тогда поздоровайся и спроси, чем ему помочь!
+    """
+
+contextualize_q_system_prompt = """Учитывая историю чата и последний вопрос пользователя,
+    который может ссылаться на контекст в истории чата, сформулируй отдельный вопрос,
+    который можно понять без истории чата. НЕ ОТВЕЧАЙ на вопрос, просто переформулируйте его,
+    если необходимо, и в противном случае верните его как есть.
+    """
 
 
 def create_table(table_name) -> None:
     # запускать только один раз
-    PostgresChatMessageHistory.create_tables(sync_connection, table_name)
+    Psql.create_tables(sync_connection, table_name)
 
 
 def drop_table(table_name) -> None:
-    PostgresChatMessageHistory.drop_table(sync_connection, table_name)
+    Psql.drop_table(sync_connection, table_name)
 
 
 def check_llm(model, question, max_tokens=10, temperature=0.0):
@@ -135,29 +156,30 @@ def get_session_history(session_id: str) -> BaseChatMessageHistory:
     Returns:
         BaseChatMessageHistory: Объект истории сообщений чата.
     """
-    return PostgresChatMessageHistory(TABLE,
-                                      session_id,
-                                      sync_connection=sync_connection)
+    # на новой либе с UUID
+    # return PostgresChatMessageHistory(TABLE,
+    #                                   session_id,
+    #                                   sync_connection=sync_connection)
+
+    # на старой либе со строковым session_id
+    return PostgresChatMessageHistory(session_id=session_id,
+                                      connection_string=CONN_STR2BD)
 
 
-def get_history_aware_retriever(llm: ChatOpenAI, vec_store_path: str | Path = VEC_STORE_LOAD_PATH) -> RunnableBinding:
+def get_history_aware_retriever(llm: ChatOpenAI, vec_store_path: str | Path,
+                                contextualize_q_system_prompt: str) -> RunnableBinding:
     """
     Создает и возвращает ретривер, учитывающий историю чата.
 
     Args:
         llm (ChatOpenAI): Языковая модель.
         vec_store_path(str): путь к векторному хранилищу базы знаний
+        contextualize_q_system_prompt: промт для работы с  историей сообщений
     Returns:
         HistoryAwareRetriever: Ретривер, который учитывает историю чата.
     """
 
     retriever = load_vectorstore(vec_store_path).as_retriever()
-
-    contextualize_q_system_prompt = """Учитывая историю чата и последний вопрос пользователя, \
-    который может ссылаться на контекст в истории чата, сформулируй отдельный вопрос, \
-    который можно понять без истории чата. НЕ ОТВЕЧАЙ на вопрос, просто переформулируйте его, \
-    если необходимо, и в противном случае верните его как есть.
-    """
 
     contextualize_q_prompt = ChatPromptTemplate.from_messages(
         [
@@ -197,26 +219,34 @@ def define_llm(api_key: str, api_base: str, model: str, max_tokens: int, tempera
     return llm
 
 
-def define_promt() -> ChatPromptTemplate:
+def define_gigachat(api_key: str, model: str, max_tokens: int, temperature: float) -> GigaChat:
+    """
+        Определяет и возвращает языковую модель.
+
+    Args:
+        api_key (str): API ключ для доступа к модели.
+        model (str): Имя модели.
+        max_tokens (int): Максимальное количество токенов для генерации.
+        temperature (float): Температура генерации текста.
+
+    Returns:
+        ChatOpenAI: Языковая модель.
+    """
+
+    llm = GigaChat(credentials=api_key,
+                   verify_ssl_certs=False,
+                   model=model,
+                   max_tokens=max_tokens,
+                   temperature=temperature)
+    return llm
+
+
+def define_promt(system_prompt: str) -> ChatPromptTemplate:
     """
     Определяет и возвращает системный промт.
 
-    Args:
-        no_memory (bool, optional): Указывает, использовать ли историю сообщений. По умолчанию False.
-
     Returns:
         ChatPromptTemplate: Системный промт.
-    """
-    system_prompt = """ Ты - чат-бот Енот, и работаешь в чате сети магазинов хороших продуктов "Жизньмарт",
-    твоя функция - стараться ответить на любой вопрос клиента про работу магазинов "Жизьмарт".
-    Используй в ответах только русский язык! Не отвечай на английском! 
-    Если вопрос не касается контекста, то вежливо и дружелюбно переведи тему и расскажи про Живчики Жизьмарта.
-
-    {context}
-
-    Используй только этот контекст, чтобы ответить на последний вопрос.
-    Если ответа нет в контексте, просто позитивно поддержи диалог на тему Жизньмарта!
-    Если клиент поздоровался с тобой, но НЕ ЗАДАЛ вопрос, тогда поздоровайся и спроси, чем ему помочь!
     """
     # Если клиент доволен ответом на вопрос, например, говорит "спасибо", скажи "спасибо" и попрощайся.
 
@@ -231,18 +261,25 @@ def define_promt() -> ChatPromptTemplate:
     return prompt
 
 
-def create_chain(vec_store_path: str | Path = VEC_STORE_LOAD_PATH, model: str = MODEL) -> RunnableWithMessageHistory:
+def create_chain(vec_store_path: str | Path = VEC_STORE_LOAD_PATH, model: str = MODEL,
+                 api_key: str = API_KEY, system_prompt: str = SYSTEM_PROMT,
+                 story_prompt: str = contextualize_q_system_prompt,
+                 max_tokens=MAX_TOKENS, temperature=TEMPERATURE) -> RunnableWithMessageHistory:
     """
         Создает и возвращает цепочку обработки запросов с учетом истории чата.
 
     Returns:
         RunnableWithMessageHistory: Цепочка обработки запросов с учетом истории чата.
     """
-    llm = define_llm(API_KEY, API_BASE, model, MAX_TOKENS, TEMPERATURE)
-    prompt = define_promt()
+    if model in ['GigaChat-Pro', 'GigaChat']:
+        llm = define_gigachat(api_key, model, max_tokens, temperature)
+    else:
+        llm = define_llm(api_key, API_BASE, model, max_tokens, temperature)
+
+    prompt = define_promt(system_prompt)
 
     doc_chain = create_stuff_documents_chain(llm, prompt)
-    history_aware_retriever = get_history_aware_retriever(llm, vec_store_path)
+    history_aware_retriever = get_history_aware_retriever(llm, vec_store_path, story_prompt)
 
     chain = create_retrieval_chain(history_aware_retriever, doc_chain)
 
